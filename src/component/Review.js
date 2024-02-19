7dfbc4b33508624131656abdd899bc8f9ade0cba
hwarim-huray <hr.ryu@huray.net>
2024-02-19 21:42:42 +0900

    [ADD]login, review component

---
+++ src/component/Review.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import styles from './review.css';
+
+
+function Review({title, surveyPeriod, reviewPeriod, state}) {
+    // const [state] = useState('TEMP');
+    // const [state] = useState('TEMP'); PROCESS PAUSE DELETED
+
+    // const [title, surveyPeriod] = useState(0);
+    return (
+
+        <div className="Review">
+            <div className="title">{title}</div>
+            <div className= "period_text_orange">{surveyPeriod.startDate} ~ {surveyPeriod.endDate} </div>
+            <div className="period">&nbsp;/&nbsp;{reviewPeriod.startDate} ~ {reviewPeriod.endDate}</div>
+            <div className="state"> {state.text}</div>
+        </div>
+    )
+}
+
+export default Review;
