1dd18ee4eacbc3158cab0bf5fd1aeb8b925208e1
dohyeon5626 <kodohyeon71@gmail.com>
2024-04-13 14:34:53 +0900

    [UPDATE] react router 추가

--- src/App.js
+++ src/App.js
@@ -6,26 +6,7 @@ import Login from "./component/Login";
 function App() {
     return (
         <div className="App">
-            <Login/>
-            {/*<Review title='밸런스 하반기 평가'*/}
-            {/*        surveyPeriod={{startDate: '2023.12.31', endDate: '2024.01.01'}}*/}
-            {/*        reviewPeriod={{startDate: '2023.12.31', endDate: '2024.01.01'}}*/}
-            {/*        state={{code:'TEMP', text:'진행 예정'}}*/}
-            {/*        />*/}
-            {/*<header className="App-header">*/}
-            {/*  /!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
-            {/*  <p>*/}
-            {/*    Edit <code>src/App.js</code> and save to reload.*/}
-            {/*  </p>*/}
-            {/*  <a*/}
-            {/*    className="App-link"*/}
-            {/*    href="https://reactjs.org"*/}
-            {/*    target="_blank"*/}
-            {/*    rel="noopener noreferrer"*/}
-            {/*  >*/}
-            {/*    Learn React*/}
-            {/*  </a>*/}
-            {/*</header>*/}
+            {/* TODO 기본 페이지 */}
         </div>
     );
 }
---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Login from './component/Login';
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
