0b239f3c9d0d1825189942aa2c82321f04471e85
홍원배 <shongs27@naver.com>
2024-08-04 19:49:30 +0900

    fix: tailwind config 설정
    *  폰트 추가
    *  버튼 스타일 변경 중
    *  칼라 기본값 설정

--- src/style/colors.ts
+++ src/style/colors.ts
@@ -1,5 +1,11 @@
 const colors = {
-  bodyGray: "#f5f7f9",
+  bodyGray: "#F5F7F9",
+  blue500: "#0277FF",
+  red500: "#E64500",
+  gray500: "#818181",
+  gray400: "#E9E9E9",
+  gray300: "#F8F8F8",
+  black400: "#2D2D2D",
 };
 
 export default colors;
