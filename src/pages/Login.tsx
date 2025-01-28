22375e6e95bd3b642865c68fd5ee24d132685946
jieunhong <hong.bbubbu@gmail.com>
2025-01-28 22:28:13 +0900

    [UPDATE] env 파일 위치 변경

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,11 +11,6 @@ import { useLogin } from "../services/login";
 function Login() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const redirect_uri =
-    process.env.REACT_APP_REDIRECT_URL || "http://localhost:3000/login";
-
-  console.log(redirect_uri);
-  console.log(process.env.REACT_APP_REDIRECT_URL);
 
   const navigate = useNavigate();
 
@@ -44,11 +39,7 @@ function Login() {
       <img className="LoginFormLogo" src={logo} alt="logo" />
       <a
         className="LoginButton"
-        href={
-          `https://accounts.google.com/o/oauth2/v2/auth?client_id=597589376829-aiiurcdf6557jok9vdkt4rukbjjb7odq.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/calendar.readonly%20https://www.googleapis.com/auth/calendar&response_type=code&access_type=offline&state=state_parameter_passthrough_value&include_granted_scopes=true&redirect_uri=` +
-          redirect_uri +
-          `&prompt=consent`
-        }
+        href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=597589376829-aiiurcdf6557jok9vdkt4rukbjjb7odq.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/calendar.readonly%20https://www.googleapis.com/auth/calendar&response_type=code&access_type=offline&state=state_parameter_passthrough_value&include_granted_scopes=true&redirect_uri=https://core-base-web.vercel.app/login&prompt=consent`}
       >
         <img src={reviewerLoginLogo} />
         Google 로그인
--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,11 +1,6 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-var baseUrl =
-  process.env.REACT_APP_API_BASE_URL ||
-  "https://core-base-5103db8db361.herokuapp.com";
-var type = process.env.REACT_APP_ACTIVE_PROFILES || "LOCAL";
-
 export function useGetRoleMenubar() {
   return useQuery({
     queryKey: ["loginUserRole"],
@@ -21,13 +16,17 @@ export function useGetRoleMenubar() {
 export function useLogin(authCode: String) {
   return useMutation({
     mutationFn: async () =>
-      await axios.post(baseUrl + "/auth/login", null, {
-        // Query 파라미터
-        params: {
-          code: authCode,
-          type: type,
-        },
-      }),
+      await axios.post(
+        "https://core-base-5103db8db361.herokuapp.com/auth/login",
+        null,
+        {
+          // Query 파라미터
+          params: {
+            code: authCode,
+            type: "REVIEWER",
+          },
+        }
+      ),
     onSuccess(data: any) {
       console.log(data);
     },
