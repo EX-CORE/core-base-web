0ac60dc2bab4826c1f52d58ebfc2a03b37cb83da
jieunhong <hong.bbubbu@gmail.com>
2025-01-29 22:29:45 +0900

    [UPDATE] profile type 추가

--- src/services/login.tsx
+++ src/services/login.tsx
@@ -1,6 +1,11 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+var baseUrl =
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://core-base-5103db8db361.herokuapp.com";
+var type = process.env.REACT_APP_ACTIVE_PROFILES || "LOCAL";
+
 export function useGetRoleMenubar() {
   return useQuery({
     queryKey: ["loginUserRole"],
@@ -16,17 +21,13 @@ export function useGetRoleMenubar() {
 export function useLogin(authCode: String) {
   return useMutation({
     mutationFn: async () =>
-      await axios.post(
-        "https://core-base-5103db8db361.herokuapp.com/auth/login",
-        null,
-        {
-          // Query 파라미터
-          params: {
-            code: authCode,
-            type: "REVIEWER",
-          },
-        }
-      ),
+      await axios.post(baseUrl + "/auth/login", null, {
+        // Query 파라미터
+        params: {
+          code: authCode,
+          type: type,
+        },
+      }),
     onSuccess(data: any) {
       console.log(data);
     },
